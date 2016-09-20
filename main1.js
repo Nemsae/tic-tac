94c9d8e3fe551057cd1b23cfe0ccca3009f048a5
John Son <jsson77@gmail.com>
2016-09-20 08:38:25 -0700

    tic-tac-toe ReactJS
    Not finished

---
+++ main1.js
@@ -0,0 +1,158 @@
+let App = React.createClass({
+  getInitialState() {
+    return {
+      player: 1,
+      winningNumbers: [['123'],['159'],['147'],['213'],['258'],['321'],['369'],['357'],['417'],['456'],['519'],['537'],['546'],['528'],['639'],['654'],['741'],['789'],['753'],['879'],['852'],['987'],['951'],['963']],
+      message: '',
+    }
+  },
+
+  changePlayer() {
+    let { player } = this.state;
+
+    if (player===2) {
+      this.setState({
+        player: 1,
+      })
+    } else {
+      this.setState({
+        player: 2,
+      })
+    }
+  },
+
+  checkGameStatus(player1Array, player2Array) {
+    let { winningNumbers, message } = this.state;
+
+    let player1String = player1Array.toString();
+    let player2String = player2Array.toString();
+
+    console.log('player1String: ',player1String);
+    console.log('player2String: ',player2String);
+
+    // console.log('winningNumbers[0]: ',winningNumbers[0]);
+    // let testArray = winningNumbers[0];
+    // console.log('typeof testArray: ',typeof(testArray));
+    // let testString = testArray.toString();
+    // let testString = testArray.toString();
+    // let newString = '['+testString+']'
+    // console.log('testString: ',testString);
+    // let testRegex = new RegExp(newString,'g');
+    // console.log('testRegex: ',testRegex);
+    //
+    // for (let i=0;i<1;i++) {
+    //   if (player1String.match(testRegex)) {
+    //     console.log('PASS')
+    //   }
+    // }
+
+
+
+    winningNumbers.forEach((array) => {
+      let newString = '['+array+']';
+      let newRegex = new RegExp(newString, 'g')
+      // console.log('Sanity: ',newRegex)
+      if (player1String.match(newRegex).length>2) {
+        console.log('Sanity3')
+        this.setState({
+          message: 'Player 1 Wins!',
+        })
+      } else if (player2String.match(newRegex).length>2) {
+        console.log('Sanity4')
+        this.setState({
+          message: 'Player 2 Wins',
+        })
+      }
+    })
+
+  },
+
+  render() {
+
+    return (
+      <div className="container">
+        <h1>Tic Tac Toe</h1>
+        <GameTable player={this.state.player} changePlayer={this.changePlayer} checkGameStatus={this.checkGameStatus} message={this.state.message}/>
+      </div>
+    )
+  }
+})
+
+let GameTable = React.createClass ({
+  getInitialState() {
+    return {
+      player1: [],
+      player2: [],
+    }
+  },
+
+  addGamePiece(e){
+    let { player, changePlayer, checkGameStatus } = this.props;
+    let { player1, player2 } = this.state;
+
+    let tdNumber = e.target.id;
+    tdNumber = parseInt(tdNumber);
+
+    if (player===1) {
+      e.target.innerHTML = 'X';
+      player1 = player1.concat(tdNumber)
+
+      this.setState({
+        player1,
+      }, changePlayer(),checkGameStatus(player1, player2))
+    } else {
+      e.target.innerHTML = 'O';
+      player2 = player2.concat(tdNumber)
+
+      this.setState({
+        player2,
+      }, changePlayer(),checkGameStatus(player1, player2))
+    }
+
+  },
+
+  // resetGame() {
+  //   let { player } = this.props;
+  //
+  //   player = 1;
+  //   for (var i=0;i<9;i++) {
+  //
+  //   }
+  //
+  //   resetGameParent(player);
+  // }
+
+  render() {
+    let { player, message } = this.props;
+    let { player1, player2 } = this.state;
+    // console.log('Child: player1 Array: ', player1, 'player2 Array: ', player2)
+
+    return (
+      <div className='container'>
+        <h3>Player {player}'s turn!</h3>
+        <div>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='1' id='1'>1</button>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='2' id='2'>2</button>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='3' id='3'>3</button>
+        </div>
+        <div>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='4' id='4'>4</button>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='5' id='5'>5</button>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='6' id='6'>6</button>
+        </div>
+        <div>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='7' id='7'>7</button>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='8' id='8'>8</button>
+          <button className='btn btn-default' onClick={this.addGamePiece} ref='9' id='9'>9</button>
+        </div>
+        {/* <button onClick={this.resetGame}>Reset</button> */}
+        <h2>{message}</h2>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
